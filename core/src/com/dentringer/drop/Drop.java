fae7f0ba923b77bd96c292e97c57bbbc30c679ab
Derek Entringer <dentringer@gmail.com>
2015-03-07 13:32:08 -0600

    added textures, sounds, music, camera matrix, sprite batch rendering

--- core/src/com/dentringer/drop/Drop.java
+++ core/src/com/dentringer/drop/Drop.java
@@ -2,26 +2,59 @@ package com.dentringer.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.awt.Rectangle;
+
 public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+    private Texture mDropImage;
+    private Texture mBucketImage;
+    private Sound mDropSound;
+    private Music mDropMusic;
+
+    private OrthographicCamera mDropCamera;
+    private SpriteBatch mSpriteBatch;
+
+    private Rectangle mRectangle;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        mDropImage = new Texture(Gdx.files.internal("droplet.png"));
+        mBucketImage = new Texture(Gdx.files.internal("bucket.png"));
+        mDropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        mDropMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        mDropMusic.setLooping(true);
+        mDropMusic.play();
+
+        mDropCamera = new OrthographicCamera();
+        mDropCamera.setToOrtho(false, 800, 480);
+
+        mSpriteBatch = new SpriteBatch();
+
+        mRectangle = new Rectangle();
+        mRectangle.x = 800/2-64/2;
+        mRectangle.y = 20;
+        mRectangle.width = 64;
+        mRectangle.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mDropCamera.update();
+
+        mSpriteBatch.setProjectionMatrix(mDropCamera.combined);
+        mSpriteBatch.begin();
+        mSpriteBatch.draw(mBucketImage, mRectangle.x, mRectangle.y);
+        mSpriteBatch.end();
 	}
 }
